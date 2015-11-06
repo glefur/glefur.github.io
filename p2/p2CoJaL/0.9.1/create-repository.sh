1b73e0488329cdd6ae6c0aa2f1213e238b033829
Goulwen Le Fur <goulwen.lefur@obeo.fr>
2015-11-06 17:01:48 +0100

    REMOVED: specific shell scripts to build p2 repository.

--- p2/p2CoJaL/0.9.1/create-repository.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-OSGITOOLS_REPO_PATH=/home/glefur/Perso/repositories/osgitools
-ECLIPSE_PATH=/home/glefur/Perso/Softwares/eclipses/mars-osgi-jee
-
-mvn integration-test
-$OSGITOOLS_REPO_PATH/scripts/repositify.sh -a $ECLIPSE_PATH $PWD/target/
-mvn clean
